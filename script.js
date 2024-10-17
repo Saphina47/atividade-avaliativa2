d23ef6cfa7d615392b4617bb7f02b25367e5626c
Jocelyn Laurent <saphina47@gmail.com>
2024-10-16 21:58:34 -0300

    atividade-avaliativa2

--- script.js
+++ script.js
@@ -1,84 +1,82 @@
-const cardValues = [
-    'A', 'A', 
-    'B', 'B', 
-    'C', 'C', 
-    'D', 'D', 
-    'E', 'E', 
-    'F', 'F', 
-    'G', 'G', 
-    'H', 'H'
-];
 
 const images = [
-    'images/image1.png',
-    'images/image1.png',
-    'images/image2.png',
-    'images/image2.png',
-    'images/image3.png',
-    'images/image3.png',
-    'images/image4.png',
-    'images/image4.png',
-    'images/image5.png',
-    'images/image5.png',
-    'images/image6.png',
-    'images/image6.png',
-    'images/image7.png',
-    'images/image7.png',
-    'images/image8.png',
-    'images/image8.png'
+  './assets/1.png', 
+  './assets/2.png',
+  './assets/3.png', 
+  './assets/4.png', 
+  './assets/5.png',
+  './assets/6.png', 
+  './assets/7.png', 
+  './assets/8.png', 
 ];
 
 let cards = [];
 let flippedCards = [];
 let matchedPairs = 0;
 
-function initGame() {
-    const gameBoard = document.getElementById('gameBoard');
-    const shuffledCards = cardValues.map((value, index) => {
-        return { value, image: images[index] };
-    }).sort(() => Math.random() - 0.5); // Embaralha as cartas
+function shuffleCards() {
+  const list = images
+    .map((value, index) => {
+      console.log(value);
+      return { value: index, image: value };
+    })
+    .sort(() => Math.random() - 0.5); // Embaralha as cartas
 
-    shuffledCards.forEach(({ value, image }) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.value = value;
+  console.log(list);
+  return list;
+}
 
-        const img = document.createElement('img');
-        img.src = image;
-        card.appendChild(img);
+function addCardsToScreen(cardsList) {
+  const gameBoard = document.getElementById("gameBoard");
+  cardsList.forEach(({ value, image }) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.value = value;
 
-        card.addEventListener('click', flipCard);
-        gameBoard.appendChild(card);
-        cards.push(card);
-    });
+    const img = document.createElement("img");
+    img.src = image;
+    card.appendChild(img);
+
+    card.addEventListener("click", flipCard);
+    gameBoard.appendChild(card);
+    cards.push(card);
+  });
 }
 
 function flipCard() {
-    if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
-        this.classList.add('flipped');
-        flippedCards.push(this);
+  if (flippedCards.length < 2 && !this.classList.contains("flipped")) {
+    this.classList.add("flipped");
+    flippedCards.push(this);
 
-        if (flippedCards.length === 2) {
-            checkForMatch();
-        }
+    if (flippedCards.length === 2) {
+      checkForMatch();
     }
+  }
 }
 
 function checkForMatch() {
-    setTimeout(() => {
-        const [firstCard, secondCard] = flippedCards;
+  setTimeout(() => {
+    const [firstCard, secondCard] = flippedCards;
 
-        if (firstCard.dataset.value === secondCard.dataset.value) {
-            matchedPairs++;
-            if (matchedPairs === cardValues.length / 2) {
-                alert('Você ganhou!');
-            }
-        } else {
-            firstCard.classList.remove('flipped');
-            secondCard.classList.remove('flipped');
-        }
-        flippedCards = [];
-    }, 1000);
+    if (firstCard.dataset.value === secondCard.dataset.value) {
+      matchedPairs++;
+      if (matchedPairs === cardValues.length / 2) {
+        alert("Você ganhou!");
+      }
+    } else {
+      firstCard.classList.remove("flipped");
+      secondCard.classList.remove("flipped");
+    }
+    flippedCards = [];
+  }, 1000);
 }
 
-window.onload = initGame;
+function initGame() {
+  const shuffledCards = [...shuffleCards(), ...shuffleCards()];
+  console.log(shuffledCards);
+  addCardsToScreen(shuffledCards);
+}
+
+
+initGame();
+
